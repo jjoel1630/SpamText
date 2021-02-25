ee7a8ff99a81468efcda7f8bd4c69cec20c6361d
Joel Johnson <jjoel1630@gmail.com>
2021-02-25 15:06:30 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import pyautogui
+import time
+import random
+
+print("what do you want to spam?")
+text = input()
+
+sleeptime = input("sleep time?\n")
+
+file = open('spamtext.txt', 'r').read()
+file_text = file.splitlines()
+
+print("how many times do you wanna spam")
+times = input()
+
+print("spam will start in 10 seconds")
+
+time.sleep(10)
+
+for x in range(0, int(times)):
+    pyautogui.typewrite(text)
+    pyautogui.press('enter')
+    pyautogui.press('enter')
+    time.sleep(float(sleeptime))
+
+print("spam completed")
